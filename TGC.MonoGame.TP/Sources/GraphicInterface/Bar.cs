fcebcee773bda740c71ecd5bd9baf2985d1a0c0b
Germán Mallet <germanmallet@gmail.com>
2021-06-08 12:21:28 -0300

    Bar

---
+++ TGC.MonoGame.TP/Sources/GraphicInterface/Bar.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace TGC.MonoGame.TP.GraphicInterface
+{
+    internal class Bar
+    {
+        private readonly Vector2 margin = new Vector2(5f, 5f);
+        private readonly Vector2 size;
+        private readonly Color color;
+        private readonly float maxValue;
+
+        internal Bar(Vector2 size, Color color, float maxValue)
+        {
+            this.size = size;
+            this.color = color;
+            this.maxValue = maxValue;
+        }
+
+        internal void Draw(Vector2 position, float value)
+        {
+            TGCGame.gui.DrawCenteredSprite(TGCGame.content.T_Pixel, position, size, new Color(0, 0, 0, 220));
+            Vector2 innerSize = size - margin * 2;
+            innerSize.X = innerSize.X * value / maxValue;
+            TGCGame.gui.DrawSprite(TGCGame.content.T_Pixel, position - size / 2 + margin, innerSize, color);
+        }
+    }
+}
