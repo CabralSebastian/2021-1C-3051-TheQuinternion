cfd56cf25763a79542bdac730b463cc965e034ab
NicoMore <nicomorelli47@gmail.com>
2021-07-06 17:45:15 -0300

    Reduccion ligera del rango de torretas

--- TGC.MonoGame.TP/Sources/ConcreteEntities/SmallTurret.cs
+++ TGC.MonoGame.TP/Sources/ConcreteEntities/SmallTurret.cs
@@ -14,7 +14,7 @@ namespace TGC.MonoGame.TP.ConcreteEntities
         protected override Drawer Drawer() => TGCGame.content.D_SmallTurret;
         protected override TypedIndex Shape => TGCGame.content.SH_SmallTurret;
 
-        protected override float MaxRange => 800f;
+        protected override float MaxRange => 600f;
         protected override float MinIdleTime => 1000f;
         protected override Vector3 CannonsOffset => new Vector3(0f, 1.70945f, 0f) * 100 * scale;
 
--- TGC.MonoGame.TP/Sources/ConcreteEntities/Turret.cs
+++ TGC.MonoGame.TP/Sources/ConcreteEntities/Turret.cs
@@ -14,7 +14,7 @@ namespace TGC.MonoGame.TP.ConcreteEntities
         protected override Drawer Drawer() => TGCGame.content.D_Turret;
         protected override TypedIndex Shape => TGCGame.content.SH_Turret;
 
-        protected override float MaxRange => 800f;
+        protected override float MaxRange => 700f;
         protected override float MinIdleTime => 1000f;
         private const float scaleFactor = 1 / 7f;
         protected override Vector3 CannonsOffset => new Vector3(0f, 2.8911f, 0f) * 100 * scale;
